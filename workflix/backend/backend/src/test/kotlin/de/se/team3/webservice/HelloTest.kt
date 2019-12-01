788c6190ec7aeeaa361682762b9b512f55075e3c
Elias Keis <elias.keis@student.uni-augsburg.de>
2019-12-01 11:54:51 +0100

    Also test backend

--- workflix/backend/backend/src/test/kotlin/de/se/team3/webservice/HelloTest.kt
+++ workflix/backend/backend/src/test/kotlin/de/se/team3/webservice/HelloTest.kt
@@ -1,5 +1,10 @@
-package webservice
+package de.se.team3.webservice
+
+import kotlin.test.Test
 
 class HelloTest {
-
+    @Test
+    fun succeedingTest() {
+        // do nothing, just to check if CI pipeline works
+    }
 }
