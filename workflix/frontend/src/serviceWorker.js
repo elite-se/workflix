f9b71394d6a7c41332b664ba9991fe2439765327
Michael Markl <marklmichael98@gmail.com>
2019-12-01 11:26:22 +0100

    Fix last flow and lint errors

--- workflix/frontend/src/serviceWorker.js
+++ workflix/frontend/src/serviceWorker.js
@@ -25,7 +25,7 @@ const isLocalhost = Boolean(
 export function register (config: any) {
   if (process.env.NODE_ENV === 'production' && navigator.serviceWorker) {
     // The URL constructor is available in all browsers that support SW.
-    const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href)
+    const publicUrl = new URL(process.env.PUBLIC_URL || '', window.location.href)
     if (publicUrl.origin !== window.location.origin) {
       // Our service worker won't work if PUBLIC_URL is on a different origin
       // from what our page is served on. This might happen if a CDN is used to
@@ -34,15 +34,15 @@ export function register (config: any) {
     }
 
     window.addEventListener('load', () => {
-      const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`
-
-      if (isLocalhost) {
+      const swUrl = `${process.env.PUBLIC_URL || ''}/service-worker.js`
+      const serviceWorker = navigator.serviceWorker
+      if (isLocalhost && serviceWorker) {
         // This is running on localhost. Let's check if a service worker still exists or not.
         checkValidServiceWorker(swUrl, config)
 
         // Add some additional logging to localhost, pointing developers to the
         // service worker/PWA documentation.
-        navigator.serviceWorker.ready.then(() => {
+        serviceWorker.ready.then(() => {
           console.log(
             'This web app is being served cache-first by a service ' +
             'worker. To learn more, visit https://bit.ly/CRA-PWA'
