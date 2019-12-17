e34a9591734742faad18d15d2d784ef157b95bbe
Erik Pallas <erik.pallas@student.uni-augsburg.de>
2019-12-18 00:08:55 +0100

    creating user from e-mail address

--- workflix/backend/backend/src/main/kotlin/de/se/team3/logic/domain/User.kt
+++ workflix/backend/backend/src/main/kotlin/de/se/team3/logic/domain/User.kt
@@ -1,5 +1,7 @@
 package de.se.team3.logic.domain
 
+import de.se.team3.logic.***REMOVED***connector.UserQuerying
+
 class User(id: String, name: String, displayname: String, email: String) {
     val id = id
     val name = name
@@ -8,7 +10,26 @@ class User(id: String, name: String, displayname: String, email: String) {
 
     constructor(name: String, displayname: String, email: String): this("", name, displayname, email) {
         if (name.length == 0 || displayname.length == 0 || email.length == 0)
-            throw IllegalArgumentException("all arugments must not be empty")
+            throw IllegalArgumentException("not all arguments may be empty")
     }
 
+    companion object {
+        //queries the ***REMOVED*** API for ***REMOVED*** Users registered under the given e-mail address
+        //throws exceptions if either the given e-mail address is not of a valid format, or no ***REMOVED*** User
+        //with this address can be found
+        fun query***REMOVED***andCreateUser(email: String): User {
+            //checks whether email is a (syntactically) valid e-mail address
+            if (!email.matches(Regex("""^\w+@\w+..{2,3}(.{2,3})?$""")))
+                throw java.lang.IllegalArgumentException("The e-mail address given is not of a valid format.")
+            val user = UserQuerying.searchFor***REMOVED***User(email)
+            if (user == null)
+                throw java.lang.IllegalArgumentException("No user with this e-mail address exists.")
+            return user
+        }
+    }
+
+}
+
+fun main() {
+    println(User.query***REMOVED***andCreateUser("e@a.pd").displayname)
 }
