61efdb5e867ad247f856c4dcc107dc819ef5bd4f
Elias Keis <elias.keis@student.uni-augsburg.de>
2019-12-19 10:22:58 +0100

    use dummy data from mockup

--- workflix/frontend/src/api/ProcessApi.js
+++ workflix/frontend/src/api/ProcessApi.js
@@ -14,62 +14,111 @@ class ProcessApi {
     }
     const taskDefault = {
       templateDescription: '',
-      taskTemplateId: 0,
       simpleClosing: true,
       personsResponsible: [],
       done: false
     }
     const proc1: ProcessType = {
       ...processDefault,
-      id: 1,
-      title: 'Process 1',
-      progress: 0.75,
+      id: 13,
+      title: 'Kapitalabruf BMW-Aktie',
+      progress: 0.4,
       tasks: [
         {
           ...taskDefault,
           taskId: 1,
-          templateName: 'Task 1',
+          templateName: 'Dokument scannen',
+          taskTemplateId: 1,
           done: true
         },
         {
           ...taskDefault,
           taskId: 2,
-          templateName: 'Task 2',
+          templateName: 'Prüfen (Admin)',
+          taskTemplateId: 2,
           done: true
         },
         {
           ...taskDefault,
           taskId: 3,
-          templateName: 'Task 3',
-          done: true
+          templateName: 'Prüfen (Asset Manager)',
+          taskTemplateId: 3
         },
         {
           ...taskDefault,
           taskId: 4,
-          templateName: 'Task 4'
+          templateName: 'Zahlung veranlassen',
+          taskTemplateId: 4
+        },
+        {
+          ...taskDefault,
+          taskId: 5,
+          templateName: 'Buchung eintragen',
+          taskTemplateId: 5
         }
       ]
     }
     const proc2: ProcessType = {
       ...processDefault,
       id: 2,
-      title: 'Process 2',
+      title: 'Dummy-Task',
       progress: 0.5,
       tasks: [
         {
           ...taskDefault,
-          taskId: 5,
-          templateName: 'Task 5',
+          taskId: 11,
+          templateName: 'Ignore this task',
+          taskTemplateId: 6,
           done: true
         },
         {
           ...taskDefault,
-          taskId: 6,
-          templateName: 'Task 6'
+          taskId: 12,
+          templateName: 'Ignore this one, too',
+          taskTemplateId: 7
         }
       ]
     }
-    return Promise.resolve([proc1, proc2])
+    const proc3: ProcessType = {
+      ...processDefault,
+      id: 15,
+      title: 'Kapitalabruf Audi-Aktie',
+      progress: 0.2,
+      tasks: [
+        {
+          ...taskDefault,
+          taskId: 6,
+          templateName: 'Dokument scannen',
+          taskTemplateId: 1,
+          done: true
+        },
+        {
+          ...taskDefault,
+          taskId: 7,
+          templateName: 'Prüfen (Admin)',
+          taskTemplateId: 2
+        },
+        {
+          ...taskDefault,
+          taskId: 8,
+          templateName: 'Prüfen (Asset Manager)',
+          taskTemplateId: 3
+        },
+        {
+          ...taskDefault,
+          taskId: 9,
+          templateName: 'Zahlung veranlassen',
+          taskTemplateId: 4
+        },
+        {
+          ...taskDefault,
+          taskId: 10,
+          templateName: 'Buchung eintragen',
+          taskTemplateId: 5
+        }
+      ]
+    }
+    return Promise.resolve([proc1, proc2, proc3])
   }
 }
 
