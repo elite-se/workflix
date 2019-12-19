abc4ea2b220f4907ff15255023afcd15d3334ce5
Erik Pallas <erik.pallas@student.uni-augsburg.de>
2019-12-19 11:06:29 +0100

    implemented creation of non-QPLIX users

--- workflix/backend/backend/src/main/kotlin/de/se/team3/logic/domain/User.kt
+++ workflix/backend/backend/src/main/kotlin/de/se/team3/logic/domain/User.kt
@@ -19,9 +19,9 @@ class User(id: String, name: String, displayname: String, email: String) {
 
     companion object {
         /*
-            queries the ***REMOVED*** API for ***REMOVED*** Users registered under the given e-mail address
-            throws exceptions if either the given e-mail address is not of a valid format, or no ***REMOVED*** User
-            with this address can be found
+        queries the ***REMOVED*** API for ***REMOVED*** Users registered under the given e-mail address
+        throws exceptions if either the given e-mail address is not of a valid format, or no ***REMOVED*** User
+        with this address can be found
         */
         fun query***REMOVED***andCreateUser(email: String): User {
             // checks whether email is a (syntactically) valid e-mail address
@@ -29,6 +29,7 @@ class User(id: String, name: String, displayname: String, email: String) {
                 throw java.lang.IllegalArgumentException("The e-mail address given is not of a valid format.")
             val user = UserQuerying.searchFor***REMOVED***User(email)
                 ?: throw java.lang.IllegalArgumentException("No user with this e-mail address exists.")
+            // checks whether the user already exists
             var i = 0
             var userList = UserDAO.getAllUsers(i, i + 20).first
             while (userList.isNotEmpty()) {
@@ -40,7 +41,40 @@ class User(id: String, name: String, displayname: String, email: String) {
             return user
         }
 
-        // TODO: create user without ***REMOVED*** querying, generating a new unique ID
+        /*
+        Creates a user not existing in ***REMOVED*** System by generating a new 24-char-ID.
+        Note: It is theoretically possible that creating a non-***REMOVED*** user while some ***REMOVED*** users are not already added
+        to the system may result in conflicting IDs. The likelihood of this, however, is fairly small.
+         */
+        fun createNewUser(name: String, displayname: String, email: String): User {
+            // checks whether email is a (syntactically) valid e-mail address
+            if (!email.matches(Regex("""^\w+@\w+..{2,3}(.{2,3})?$""")))
+                throw java.lang.IllegalArgumentException("The e-mail address given is not of a valid format.")
+            var generatedID = ""
+            do {
+                val charPool: List<Char> = ('a'..'z') + ('A'..'Z') + ('0'..'9')
+                generatedID = (1..24)
+                    .map { i -> kotlin.random.Random.nextInt(0, charPool.size) }
+                    .map(charPool::get)
+                    .joinToString("");
+            } while ( !idAlreadyUsed(generatedID) )
+            return User(generatedID, name, displayname, email)
+        }
+
+        /*
+        checks whether a String is already in use as an user id
+        //TODO(improvement) apply a faster searching algorithm; this may however require sorting the data first
+         */
+        fun idAlreadyUsed(id: String): Boolean {
+            var i = 0
+            var userList = UserDAO.getAllUsers(i, i + 20).first
+            while (userList.isNotEmpty()) {
+                for (user in userList)
+                    if (user.id == id) return true
+                userList = UserDAO.getAllUsers(i, i + 20).first
+            }
+            return false
+        }
     }
 }
 
