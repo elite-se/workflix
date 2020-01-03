a09a1a3efaac5aaf0dfabebb84963752e62534db
Michael Markl <marklmichael98@gmail.com>
2020-01-03 20:47:20 +0100

    Add task description to task template editor

--- workflix/frontend/src/routes/create-process-template/components/TaskTemplateEditor.js
+++ workflix/frontend/src/routes/create-process-template/components/TaskTemplateEditor.js
@@ -7,6 +7,7 @@ import { difference } from 'lodash'
 import PredecessorSelect from './PredecessorSelect'
 import SuccessorSelect from './SuccessorSelect'
 import styled from 'styled-components'
+import AutoSizeTextArea from '../../../modules/common/AutoSizeTextArea'
 
 type PropsType = {
   task: TaskTemplateType,
@@ -20,7 +21,7 @@ type StateType = {
 }
 
 const TrashButton = styled(Button)`
-  margin-top: 40px;
+  margin-top: 20px;
 `
 
 const findAncestors = (task: TaskTemplateType, allTasks: TaskTemplateType[]) => {
@@ -57,6 +58,13 @@ class TaskTemplateEditor extends React.Component<PropsType, StateType> {
     })
   }
 
+  onDescriptionChange = (event: SyntheticInputEvent<HTMLInputElement>) => {
+    this.props.onChange({
+      ...this.props.task,
+      description: event.target.value
+    })
+  }
+
   onOpenDeleteAlert = () => this.setState({ deleteAlertOpen: true })
   onCloseDeleteAlert = () => this.setState({ deleteAlertOpen: false })
 
@@ -76,30 +84,38 @@ class TaskTemplateEditor extends React.Component<PropsType, StateType> {
       flex: 1
     }}>
       <H3>Edit Task Template</H3>
-      <div>
+      <p>
         <H4>Name:</H4>
         <InputGroup type='text' placeholder='Name...'
                     value={this.props.task.name}
                     fill
                     onChange={this.onTitleChange}/>
-      </div>
-      <div>
+      </p>
+      <p>
+        <H4>Description:</H4>
+        <AutoSizeTextArea placeholder={'Add description...\n\nWhat should be done?\nWhat needs special attention?'}
+                          value={this.props.task.description}
+                          style={{ resize: 'vertical' }}
+                          className='bp3-fill' minRows={4}
+                          onChange={this.onDescriptionChange}/>
+      </p>
+      <p>
         <H4>Duration:</H4>
         <InputGroup type='number' placeholder='Duration...'
                     value={this.props.task.estimatedDuration}
                     fill
                     onChange={this.onDurationChange} min={0.1} step={0.1}/>
-      </div>
-      <div>
+      </p>
+      <p>
         <H4>Predecessor tasks:</H4>
         <PredecessorSelect allTasks={allTasks} onChange={onChange} possiblePreds={possiblePreds} task={task}/>
-      </div>
-      <div>
+      </p>
+      <p>
         <H4>Successor tasks:</H4>
         <SuccessorSelect allTasks={allTasks} succs={succs} onChange={onChange} possibleSuccs={possibleSuccs}
                          task={task}/>
-      </div>
-      <div style={{ textAlign: 'center' }}>
+      </p>
+      <p style={{ textAlign: 'center' }}>
         <TrashButton icon='trash' text='Delete Task Template' intent='danger' onClick={this.onOpenDeleteAlert}/>
         <Alert isOpen={deleteAlertOpen} icon='trash' intent='danger' confirmButtonText='Delete' canEscapeKeyCancel
                canOutsideClickCancel cancelButtonText='Cancel' onConfirm={this.props.onDelete}
@@ -110,7 +126,7 @@ class TaskTemplateEditor extends React.Component<PropsType, StateType> {
             All dependencies of it will be removed as well.
           </p>
         </Alert>
-      </div>
+      </p>
     </div>
   }
 }
