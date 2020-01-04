b6c19b8a0b3bc876663d613241b2a0f32cd540f9
Michael Markl <marklmichael98@gmail.com>
2020-01-04 17:29:26 +0100

    Add something I won't fix

--- workflix/frontend/src/Header.js
+++ workflix/frontend/src/Header.js
@@ -5,11 +5,27 @@ import { Button, Navbar } from '@blueprintjs/core'
 import { Alignment } from '@blueprintjs/core/lib/cjs/common/alignment'
 import { Link } from '@reach/router'
 
-class Header extends React.Component<{}> {
+const QUARTER_HOUR = 900000
+const THOUSAND = 1000
+
+class Header extends React.Component<{}, { title: React$Node }> {
+  state = { title: 'Workflix' }
+
+  componentDidMount () {
+    setTimeout(this.wontfix, QUARTER_HOUR)
+  }
+
+  wontfix = () => {
+    this.setState(state => ({
+      title: state.title === 'Workflix' ? <span style={{ fontSize: '1.04em' }}>Won'tfix</span> : 'Workflix'
+    }))
+    setTimeout(this.wontfix, THOUSAND * Math.random())
+  }
+
   render () {
     return <Navbar>
       <Navbar.Group align={Alignment.LEFT}>
-        <Navbar.Heading>Workflix</Navbar.Heading>
+        <Navbar.Heading>{this.state.title}</Navbar.Heading>
         <Navbar.Divider/>
         <Link to='/'><Button className='bp3-minimal' icon='layers' text='Tasks overview'/></Link>
         <Link to='process-templates'><Button className='bp3-minimal' icon='new-layers' text='Process templates'/></Link>
