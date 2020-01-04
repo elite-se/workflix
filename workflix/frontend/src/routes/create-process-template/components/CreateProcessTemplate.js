3e54eafac2fe158707bd98d13aa1255bcd843dc2
Michael Markl <marklmichael98@gmail.com>
2020-01-04 11:52:16 +0100

    Add task description duration limit and owner to process

--- workflix/frontend/src/routes/create-process-template/components/CreateProcessTemplate.js
+++ workflix/frontend/src/routes/create-process-template/components/CreateProcessTemplate.js
@@ -3,11 +3,16 @@
 import React from 'react'
 import ProcessChart from './ProcessChart'
 import { times } from 'lodash'
-import { Drawer, H2 } from '@blueprintjs/core'
+import { Button, Drawer, H2, InputGroup, Label } from '@blueprintjs/core'
 import TaskList from './TaskList'
 import MOCK_TASK_TEMPLATES from './mockTasks'
 import TaskTemplateEditor from './TaskTemplateEditor'
 import type { TaskTemplateType } from '../../../modules/datatypes/Task'
+import AutoSizeTextArea from '../../../modules/common/AutoSizeTextArea'
+import type { UserType } from '../../../modules/datatypes/User'
+import UserApi from '../../../modules/api/UsersApi'
+import withPromiseResolver from '../../../modules/app/hocs/withPromiseResolver'
+import UserSelect from './UserSelect'
 
 export type ProcessedTaskTemplateType = {|
   ...TaskTemplateType,
@@ -37,16 +42,28 @@ const calcEndDates = (nodes: Array<TaskTemplateType>): Array<ProcessedTaskTempla
 
 type StateType = {
   tasks: Array<TaskTemplateType>,
-  selectedTaskId: ?number
+  selectedTaskId: ?number,
+  title: string,
+  description: string,
+  durationLimit: ?number,
+  owner: ?UserType
 }
 
-class CreateProcessTemplate extends React.Component<{}, StateType> {
+type PropsType = {
+  users: Map<string, UserType>
+}
+
+class CreateProcessTemplate extends React.Component<PropsType, StateType> {
   state = {
     tasks: MOCK_TASK_TEMPLATES,
-    selectedTaskId: null
+    selectedTaskId: null,
+    durationLimit: null,
+    description: '',
+    title: '',
+    owner: null
   }
 
-  editTask = (id: number) => {
+  selectTaskId = (id: number) => {
     this.setState({ selectedTaskId: id })
   }
 
@@ -62,7 +79,7 @@ class CreateProcessTemplate extends React.Component<{}, StateType> {
       const newTask: TaskTemplateType = {
         id: newId,
         predecessors: [],
-        name: 'New Task',
+        name: '',
         estimatedDuration: 1,
         description: '',
         necessaryClosings: 0
@@ -98,20 +115,99 @@ class CreateProcessTemplate extends React.Component<{}, StateType> {
       title={task?.name || ''}
       onClose={this.unselectTask}
       style={{ overflow: 'auto' }}>
-      {task && <TaskTemplateEditor task={task} onChange={this.taskChanged} allTasks={tasks} onDelete={this.onDeleteTask}/>}
+      {task &&
+      <TaskTemplateEditor task={task} onChange={this.taskChanged} allTasks={tasks} onDelete={this.onDeleteTask}/>}
     </Drawer>
   }
 
+  onTitleChange = (event: SyntheticInputEvent<HTMLInputElement>) => this.setState(
+    { title: event.target.value }
+  )
+
+  onDescriptionChange = (event: SyntheticInputEvent<HTMLInputElement>) => this.setState(
+    { description: event.target.value }
+  )
+
+  onDurationLimitChange = (event: SyntheticInputEvent<HTMLInputElement>) => this.setState(
+    { durationLimit: event.target.value ? Number(event.target.value) : null }
+  )
+
+  onOwnerChange = (owner: UserType) => this.setState({ owner })
+
   render () {
-    const processedNodes = calcEndDates(this.state.tasks)
+    const { tasks, title, description, durationLimit, owner, selectedTaskId } = this.state
+    const { users } = this.props
+    const processedNodes = calcEndDates(tasks)
     return <div style={{
       flex: 1,
       display: 'flex',
-      flexDirection: 'column'
+      flexDirection: 'column',
+      marginRight: selectedTaskId !== null ? Drawer.SIZE_SMALL : '0',
+      transition: 'margin-right 0.3s'
     }}>
-      <H2 style={{ textAlign: 'center' }}>Create a new Process Template</H2>
+      <div style={{
+        display: 'flex',
+        flowDirection: 'row',
+        marginBottom: '10px',
+        justifyContent: 'start',
+        alignItems: 'center'
+      }}>
+        <Button icon='floppy-disk' text='Save' intent='primary'/>
+        <H2 style={{
+          display: 'inline',
+          marginLeft: '40px'
+        }}>Create Process Template</H2>
+      </div>
       <div style={{ display: 'flex' }}>
-        <TaskList taskTemplates={processedNodes} createTask={this.createTask} editTask={this.editTask}/>
+        <Label>
+          Title
+          <InputGroup large style={{ minWidth: '500px' }} onChange={this.onTitleChange} value={title}
+                      placeholder='Add process template title...'/>
+        </Label>
+      </div>
+      <div style={{
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-between'
+      }}>
+        <div style={{
+          display: 'flex',
+          flexDirection: 'column',
+          width: '30%'
+        }}>
+          <Label>Description
+            <AutoSizeTextArea
+              style={{
+                resize: 'none'
+              }}
+              minRows={4}
+              className='bp3-fill'
+              onChange={this.onDescriptionChange}
+              value={description}
+              placeholder={'Add description...\n\nWhat is this process about?\nWhen should it be initiated?'}/>
+          </Label>
+        </div>
+        <div style={{
+          display: 'flex',
+          flexDirection: 'column',
+          width: '30%'
+        }}>
+          <Label>Duration limit
+            <InputGroup
+              type='number'
+              style={{ resize: 'none' }}
+              onChange={this.onDurationLimitChange}
+              value={durationLimit}/>
+          </Label>
+          <Label>Owner
+            <UserSelect users={Array.from(users.values())} activeItem={owner} onItemSelect={this.onOwnerChange}/>
+          </Label>
+        </div>
+        <div style={{ width: '30%' }}/>
+      </div>
+      <div style={{ display: 'flex' }}>
+        <TaskList selectedId={selectedTaskId} taskTemplates={processedNodes} createTask={this.createTask}
+                  selectTaskId={this.selectTaskId}/>
         <ProcessChart tasks={processedNodes}/>
       </div>
       {this.renderTaskTemplateEditor()}
@@ -119,4 +215,5 @@ class CreateProcessTemplate extends React.Component<{}, StateType> {
   }
 }
 
-export default CreateProcessTemplate
+const promiseCreator = () => new UserApi().getUsers().then(users => ({ users }))
+export default withPromiseResolver<*, *>(promiseCreator)(CreateProcessTemplate)
