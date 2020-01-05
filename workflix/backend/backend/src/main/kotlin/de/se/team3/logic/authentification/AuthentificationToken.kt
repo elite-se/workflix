db99234711bc55caceb0f3670f305aad7ff1e343
Erik Pallas <erik.pallas@student.uni-augsburg.de>
2020-01-05 17:13:29 +0100

    logic log in

--- workflix/backend/backend/src/main/kotlin/de/se/team3/logic/authentification/AuthentificationToken.kt
+++ workflix/backend/backend/src/main/kotlin/de/se/team3/logic/authentification/AuthentificationToken.kt
@@ -1,9 +1,10 @@
 package de.se.team3.logic.authentification
 
 import de.se.team3.logic.domain.User
-import java.util.*
+import java.util.Base64
 
-class AuthentificationToken(val token: String) {
+class AuthentificationToken(val token: String,
+                            val user: User) {
 
     /**
      * Creates an authentification token for a given user.
@@ -11,5 +12,5 @@ class AuthentificationToken(val token: String) {
      * @param user user whose token will be generated
      */
     constructor(user: User):
-            this(Base64.getEncoder().encodeToString(user.id.toByteArray()))
+            this(Base64.getEncoder().encodeToString(user.id.toByteArray()), user)
 }
--- workflix/backend/backend/src/main/kotlin/de/se/team3/logic/authentification/LoginManager.kt
+++ workflix/backend/backend/src/main/kotlin/de/se/team3/logic/authentification/LoginManager.kt
@@ -1,17 +1,37 @@
 package de.se.team3.logic.authentification
 
+import de.se.team3.logic.container.UserContainer
+import de.se.team3.logic.domain.User
+import de.se.team3.logic.exceptions.InvalidInputException
+
 object LoginManager {
+    val loggedInUsers = ArrayList<User>()
+    val tokensInUse = ArrayList<AuthentificationToken>()
 
     /**
-     * Logs a user in if username and password match the saved information.
+     * Logs a user in if email and password match the saved information.
      *
-     * @param username currently the users email address
+     * @param email The users email address. Note that these email addresses are considered unique.
      * @param password user's password
      *
      * @return User's authentification token if successful, null otherwise.
+     *
+     * @throws InvalidInputException if there is no or more than one user with this email address, or the user
+     * is already logged in.
      */
-    fun login(username: String, password: String): AuthentificationToken? {
-        TODO()
+    fun login(email: String, password: String): AuthentificationToken? {
+        val userList = UserContainer.getAllUsers().filter { it.email == email }
+        //if there are more than two users with the same email address, something went terribly wrong during user creation
+        if (userList.size < 1)
+            throw InvalidInputException("There is no user with this email address.")
+        if (userList.size > 1)
+            throw InvalidInputException("There is more than one user with the same email address.")
+        val user = userList.first()
+        if (loggedInUsers.contains(user))
+            throw InvalidInputException("This user is already logged in.")
+        val token = AuthentificationToken(user)
+        tokensInUse.add(token)
+        return token
     }
 
     /**
@@ -19,7 +39,8 @@ object LoginManager {
      *
      * @param username name of the user to be logged out (i.e., currently the users email address)
      */
-    fun logout(username: String) {
-        TODO()
+    fun logout(token: AuthentificationToken) {
+        loggedInUsers.remove(token.user)
+        tokensInUse.remove(token)
     }
 }
