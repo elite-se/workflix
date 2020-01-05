f4a966ccc2453fd7f61a1193008610a2b1cf0898
Elias Keis <elias.keis@student.uni-augsburg.de>
2020-01-05 13:19:41 +0100

    improve process cards

--- workflix/frontend/src/routes/tasks/components/TaskSummaryCard.js
+++ workflix/frontend/src/routes/tasks/components/TaskSummaryCard.js
@@ -1,19 +1,19 @@
 // @flow
 
 import React from 'react'
-import type { TaskTemplateType, TaskType } from '../../../modules/datatypes/Task'
+import type { TaskStateType, TaskTemplateType, TaskType } from '../../../modules/datatypes/Task'
 import { Card, Colors } from '@blueprintjs/core'
 import styled from 'styled-components'
 import { Elevation as ELEVATION } from '@blueprintjs/core/lib/cjs/common/elevation'
 import type { UserType } from '../../../modules/datatypes/User'
 
-const FinishedTaskStyling = styled<{ taskFinished: boolean }, {}, 'div'>('div')`
-  margin: 3px;
-  ${props => props.taskFinished ? `
+const FinishedTaskStyling = styled<{ status: TaskStateType }, {}, 'div'>('div')`
+  margin: 3px 0;
+  ${props => props.status !== 'BLOCKED' && `
   & > * {
-    background: ${Colors.GREEN4};
+    border: 1px solid ${props.status === 'CLOSED' ? Colors.GREEN4 : Colors.ORANGE4};
   }
-` : ''}
+`}
 `
 
 type PropsType = {
@@ -30,19 +30,19 @@ class TaskSummaryCard extends React.Component<PropsType> {
   render () {
     const task = this.props.task
     const taskTemplate = this.props.taskTemplates.get(task.taskTemplateId)
-    return <FinishedTaskStyling taskFinished={task.status === 'CLOSED'}>
+    return <FinishedTaskStyling status={task.status}>
       <Card interactive elevation={this.props.selected ? ELEVATION.FOUR : undefined}
             onClick={this.onClick}>
-        <p><b>{taskTemplate ? taskTemplate.name : ''}</b></p>
-        <p>{task.assignments.length === 0 ? 'No Assignees'
-          : <span>Assigned to: {
+        <span><b>{taskTemplate ? taskTemplate.name : ''}</b></span><br/>
+        <small>{task.assignments.length === 0 ? 'no assignees'
+          : <span>{
             task.assignments.map(assignee => {
               const user = this.props.users.get(assignee.assigneeId)
               return <span key={assignee.assigneeId} className='comma'>
                 {user ? user.name : assignee.assigneeId}
               </span>
             })
-          }</span>}</p>
+          }</span>}</small>
       </Card>
     </FinishedTaskStyling>
   }
