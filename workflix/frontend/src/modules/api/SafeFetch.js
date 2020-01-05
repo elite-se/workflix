39f2487fc7f54b4614b55aea6466199bbb285731
Michael Markl <marklmichael98@gmail.com>
2020-01-05 23:51:56 +0100

    add token to headers in safeFetch

--- workflix/frontend/src/modules/api/SafeFetch.js
+++ workflix/frontend/src/modules/api/SafeFetch.js
@@ -1,7 +1,34 @@
 // @flow
 
+import { getToken } from '../common/tokenStorage'
+
+const AUTH_HEADER = 'Authorization'
+
+type MyOptionsType = {
+  ...$Diff<RequestOptions, { headers?: HeadersInit }>,
+  headers?: { [key: string]: string }
+}
+
+const addTokenHeader = (init?: MyOptionsType): MyOptionsType => {
+  const token = getToken()
+  if (!token) {
+    return init || {}
+  }
+  if (!init || !init.headers) {
+    return { ...init, headers: { [AUTH_HEADER]: token } }
+  }
+  const headers: { [key: string]: string } = init.headers
+  return {
+    ...init,
+    headers: {
+      ...headers,
+      [AUTH_HEADER]: token
+    }
+  }
+}
+
 export const safeFetch = async (input: RequestInfo, init?: RequestOptions) => {
-  const response = await fetch(input, init)
+  const response = await fetch(input, addTokenHeader(init))
   if (!response.ok) {
     const text = await response.text()
     throw new Error(text)
