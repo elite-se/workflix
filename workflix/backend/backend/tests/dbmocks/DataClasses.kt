e4a09e9cfa2c55ee74f6a9b3935266db7d6cb4c9
Erik Pallas <erik.pallas@student.uni-augsburg.de>
2020-01-05 21:08:57 +0100

    add data classes

--- workflix/backend/backend/tests/dbmocks/DataClasses.kt
+++ workflix/backend/backend/tests/dbmocks/DataClasses.kt
@@ -4,7 +4,52 @@ import java.time.Instant
 
 class DataClasses {
 
-    data class UserData(
+    data class TaskCommentsRow(
+        var id: Int,
+        var taskId: Int,
+        var creatorId: String,
+        var content: String,
+        var createdAd: java.time.Instant,
+        var deleted: Boolean
+    )
+
+    data class TaskTemplateRelationshipsRow(
+        var id: Int,
+        var predecessor: Int,
+        var successor: Int
+    )
+
+    data class TaskTemplatesRow(
+        var id: Int,
+        var processTemplateId: Int,
+        var name: String,
+        var estimatedDuration: Int,
+        var description: String,
+        var necessaryClosings: Int
+    )
+
+    data class TasksRow(
+        var id: Int,
+        var processId: Int,
+        var taskTemplateId: Int,
+        var startedAt: java.time.Instant
+    )
+
+    data class UserRoleMembersRow(
+        var id: Int,
+        var userId: String,
+        var userRoleId: Int
+    )
+
+    data class UserRolesRow(
+        var id: Int,
+        var name: String,
+        var description: String,
+        var createdAt: Instant,
+        var deleted: Boolean
+    )
+
+    data class UsersRow(
         var id: String,
         var name: String,
         var displayname: String,
@@ -13,7 +58,5 @@ class DataClasses {
         var createdAt: Instant,
         var password: String
     )
-
-
 
 }
