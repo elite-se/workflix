5e88721e89e5b41d50e346d7f1afc3adfe24af94
Marvin Brieger <mail@marvinbrieger.de>
2020-01-08 13:03:17 +0100

    testing process template

--- workflix/backend/backend/tests/unittests/ProcessTemplateTest.kt
+++ workflix/backend/backend/tests/unittests/ProcessTemplateTest.kt
@@ -1,112 +1,53 @@
 package unittests
 
-import de.se.team3.logic.domain.TaskTemplate
-import de.se.team3.logic.domain.User
-import de.se.team3.logic.domain.UserRole
-import de.se.team3.logic.domain.processTemplateUtil.ProcessTemplateCycleDetection
 import org.junit.Test
-import java.time.Instant
 import kotlin.test.assertEquals
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
 
 class ProcessTemplateTest {
 
     @Test
-    fun testCycleDetectionUnconnectedTaskTemplates() {
-        val accountantRole = TestData.getBuchhalter()
+    fun testEstimatedDurationSum() {
+        val processTemplate1 = TestData.getProcessTemplate1()
+        assertEquals(10, processTemplate1.getEstimatedDurationSum())
 
-        val taskTemplates = HashMap<Int, TaskTemplate>()
-        val taskTemplate1 = TaskTemplate(1, accountantRole, "TaskTemplate1", "Description", 1, 1)
-        taskTemplates.put(1, taskTemplate1)
-        val taskTemplate2 = TaskTemplate(2, accountantRole, "TaskTemplate2", "Description", 1, 1)
-        taskTemplates.put(2, taskTemplate2)
-        val taskTemplate3 = TaskTemplate(3, accountantRole, "TaskTemplate3", "Description", 1, 1)
-        taskTemplates.put(3, taskTemplate3)
-
-        assertEquals(true, ProcessTemplateCycleDetection.isAcyclic(taskTemplates))
+        val processTemplate2 = TestData.getProcessTemplate2()
+        assertEquals(20, processTemplate2.getEstimatedDurationSum())
     }
 
     @Test
-    fun testCycleDetectionAcyclic1() {
-        val accountantRole = TestData.getBuchhalter()
+    fun testUsesUserRole() {
+        val processTemplate = TestData.getProcessTemplate1()
+        val accountant = TestData.getAccountant()
+        val investmentManager = TestData.getInvestmentManager()
+        val unusedUser = TestData.getUnusedUserRole()
 
-        val taskTemplates = HashMap<Int, TaskTemplate>()
-        val taskTemplate1 = TaskTemplate(1, accountantRole, "TaskTemplate1", "Description", 1, 1)
-        taskTemplates.put(1, taskTemplate1)
-        val taskTemplate2 = TaskTemplate(2, accountantRole, "TaskTemplate2", "Description", 1, 1)
-        taskTemplates.put(2, taskTemplate2)
-        val taskTemplate3 = TaskTemplate(3, accountantRole, "TaskTemplate3", "Description", 1, 1)
-        taskTemplates.put(3, taskTemplate3)
-        val taskTemplate4 = TaskTemplate(4, accountantRole, "TaskTemplate4", "Description", 1, 1)
-        taskTemplates.put(4, taskTemplate4)
-        val taskTemplate5 = TaskTemplate(5, accountantRole, "TaskTemplate5", "Description", 1, 1)
-        taskTemplates.put(5, taskTemplate5)
-
-        taskTemplate2.addPredecessor(taskTemplate1)
-        taskTemplate3.addPredecessor(taskTemplate1)
-        taskTemplate4.addPredecessor(taskTemplate2)
-        taskTemplate4.addPredecessor(taskTemplate3)
-        taskTemplate5.addPredecessor(taskTemplate4)
-
-        assertEquals(true, ProcessTemplateCycleDetection.isAcyclic(taskTemplates))
+        assertTrue { processTemplate.usesUserRole(accountant) }
+        assertTrue { processTemplate.usesUserRole(investmentManager) }
+        assertFalse { processTemplate.usesUserRole(unusedUser) }
     }
 
     @Test
-    fun testCycleDetectionAcyclic2() {
-        val accountantRole = TestData.getBuchhalter()
+    fun testIsOneResponsible() {
+        val processTemplate = TestData.getProcessTemplate1()
 
-        val taskTemplates = HashMap<Int, TaskTemplate>()
-        val taskTemplate1 = TaskTemplate(1, accountantRole, "TaskTemplate1", "Description", 1, 1)
-        taskTemplates.put(1, taskTemplate1)
-        val taskTemplate2 = TaskTemplate(2, accountantRole, "TaskTemplate2", "Description", 1, 1)
-        taskTemplates.put(2, taskTemplate2)
-        val taskTemplate3 = TaskTemplate(3, accountantRole, "TaskTemplate3", "Description", 1, 1)
-        taskTemplates.put(3, taskTemplate3)
-        val taskTemplate4 = TaskTemplate(4, accountantRole, "TaskTemplate4", "Description", 1, 1)
-        taskTemplates.put(4, taskTemplate4)
-        val taskTemplate5 = TaskTemplate(5, accountantRole, "TaskTemplate5", "Description", 1, 1)
-        taskTemplates.put(5, taskTemplate5)
+        val responsibles1 = ArrayList<Int>()
+        responsibles1.add(1)
+        responsibles1.add(2)
 
-        taskTemplate3.addPredecessor(taskTemplate1)
-        taskTemplate3.addPredecessor(taskTemplate2)
-        taskTemplate4.addPredecessor(taskTemplate3)
-        taskTemplate5.addPredecessor(taskTemplate3)
+        assertTrue { processTemplate.isOneResponsible(responsibles1) }
 
-        assertEquals(true, ProcessTemplateCycleDetection.isAcyclic(taskTemplates))
-    }
+        val responsibles2 = ArrayList<Int>()
+        responsibles2.add(3)
+        responsibles2.add(4)
 
-    @Test
-    fun testCycleDetectionSingleLoop() {
-        val accountantRole = TestData.getBuchhalter()
+        assertTrue { processTemplate.isOneResponsible(responsibles2) }
 
-        val taskTemplates = HashMap<Int, TaskTemplate>()
-        val taskTemplate1 = TaskTemplate(1, accountantRole, "TaskTemplate1", "Description", 1, 1)
-        taskTemplates.put(1, taskTemplate1)
+        val responsibles3 = ArrayList<Int>()
+        responsibles2.add(4)
 
-        taskTemplate1.addPredecessor(taskTemplate1)
-
-        assertEquals(false, ProcessTemplateCycleDetection.isAcyclic(taskTemplates))
-    }
-
-    @Test
-    fun testCycleDetectionGreaterLoop() {
-        val accountantRole = TestData.getBuchhalter()
-
-        val taskTemplates = HashMap<Int, TaskTemplate>()
-        val taskTemplate1 = TaskTemplate(1, accountantRole, "TaskTemplate1", "Description", 1, 1)
-        taskTemplates.put(1, taskTemplate1)
-        val taskTemplate2 = TaskTemplate(2, accountantRole, "TaskTemplate2", "Description", 1, 1)
-        taskTemplates.put(2, taskTemplate2)
-        val taskTemplate3 = TaskTemplate(3, accountantRole, "TaskTemplate3", "Description", 1, 1)
-        taskTemplates.put(3, taskTemplate3)
-        val taskTemplate4 = TaskTemplate(4, accountantRole, "TaskTemplate4", "Description", 1, 1)
-        taskTemplates.put(4, taskTemplate4)
-
-        taskTemplate2.addPredecessor(taskTemplate1)
-        taskTemplate3.addPredecessor(taskTemplate2)
-        taskTemplate4.addPredecessor(taskTemplate3)
-        taskTemplate1.addPredecessor(taskTemplate4)
-
-        assertEquals(false, ProcessTemplateCycleDetection.isAcyclic(taskTemplates))
+        assertFalse { processTemplate.isOneResponsible(responsibles3) }
     }
 
 }
--- workflix/backend/backend/tests/unittests/TestData.kt
+++ workflix/backend/backend/tests/unittests/TestData.kt
@@ -1,5 +1,8 @@
 package unittests
 
+import de.se.team3.logic.domain.ProcessTemplate
+import de.se.team3.logic.domain.Task
+import de.se.team3.logic.domain.TaskTemplate
 import de.se.team3.logic.domain.User
 import de.se.team3.logic.domain.UserRole
 import java.time.Instant
@@ -9,6 +12,11 @@ object TestData {
     val marvinBrieger = User("58c120552c94decf6cf3b700", "Marvin Brieger", "MB", "mail@marvinbrieger.de", "Passwort123", Instant.now())
     val eliasKeis = User("58c120552c94decf6cf3b701", "Elias Keis", "EK", "ek@gmx.net", "Passwort123", Instant.now())
     val michaelMarkl = User("58c120552c94decf6cf3b702", "Michael Markl", "MK", "mk@gmail.com", "Passwort123", Instant.now())
+    val erikPallas = User("58c120552c94decf6cf3b703", "Erik Pallas", "EP", "ep@web.de", "Passwort123", Instant.now())
+    val karlCustomerAdvisor = User("58c120552c94decf6cf3b704", "Karl Customer Advisor", "KA", "ka@mfo.de", "Passwort123", Instant.now())
+    val kunigundeCustomerAdvisor = User("58c120552c94decf6cf3b705", "Kunigunde Customer Advisor", "KuA", "kua@mfo.de", "Passwort123", Instant.now())
+    val unusedUser = User("58c120552c94decf6cf3b706", "Ursula Unused", "UU", "uu@notrelevant.com", "Passwort123", Instant.now())
+
 
     fun getSomeUsers(): List<User> {
         val users = ArrayList<User>()
@@ -22,13 +30,116 @@ object TestData {
 
     }
 
-    fun getBuchhalter(): UserRole {
+    fun getAccountant(): UserRole {
         val accountants = ArrayList<User>()
         accountants.add(marvinBrieger)
         accountants.add(eliasKeis)
+        accountants.add(michaelMarkl)
 
         val accountantRole = UserRole(1, "Accountant", "Role for accountants", Instant.now(), false, accountants)
         return accountantRole
+    }
+
+    fun getCustomerAdvisor(): UserRole {
+        val customerAdvisors = ArrayList<User>()
+        customerAdvisors.add(karlCustomerAdvisor)
+        customerAdvisors.add(kunigundeCustomerAdvisor)
+
+        val customerAdvisorRole = UserRole(2, "Customer Advisor", "Role for customer advisors", Instant.now(), false, customerAdvisors)
+        return customerAdvisorRole
+    }
+
+    fun getInvestmentManager(): UserRole {
+        val investmentManagers = ArrayList<User>()
+        investmentManagers.add(erikPallas)
+
+        val investementManagerRole = UserRole(3, "Invenstment Manager", "Role for investment managers", Instant.now(), false, investmentManagers)
+        return investementManagerRole
+    }
+
+    fun getUnusedUserRole(): UserRole {
+        val usuedUsers = ArrayList<User>()
+        usuedUsers.add(unusedUser)
+
+        val unusedRole = UserRole(4, "Unused", "Unused Role", Instant.now(), false, usuedUsers)
+        return unusedRole
+    }
+
+    fun getTaskTemplatesSet1(): Map<Int, TaskTemplate> {
+        val taskTemplates = HashMap<Int, TaskTemplate>()
+
+        val taskTemplate1 = TaskTemplate(1, getCustomerAdvisor(), "TaskTemplate1", "Description", 1, 1)
+        taskTemplates.put(1, taskTemplate1)
+        val taskTemplate2 = TaskTemplate(2, getInvestmentManager(), "TaskTemplate2", "Description", 1, 1)
+        taskTemplates.put(2, taskTemplate2)
+        val taskTemplate3 = TaskTemplate(3, getAccountant(), "TaskTemplate3", "Description", 2, 1)
+        taskTemplates.put(3, taskTemplate3)
+        val taskTemplate4 = TaskTemplate(4, getAccountant(), "TaskTemplate4", "Description", 3, 1)
+        taskTemplates.put(4, taskTemplate4)
+        val taskTemplate5 = TaskTemplate(5, getCustomerAdvisor(), "TaskTemplate5", "Description", 3, 1)
+        taskTemplates.put(5, taskTemplate5)
+
+        taskTemplate2.addPredecessor(taskTemplate1)
+        taskTemplate3.addPredecessor(taskTemplate1)
+        taskTemplate4.addPredecessor(taskTemplate2)
+        taskTemplate4.addPredecessor(taskTemplate3)
+        taskTemplate5.addPredecessor(taskTemplate4)
+
+        return taskTemplates
+    }
+
+    fun getTaskTemplatesSet2(): Map<Int, TaskTemplate> {
+        val taskTemplates = HashMap<Int, TaskTemplate>()
+
+        val taskTemplate1 = TaskTemplate(1, getCustomerAdvisor(), "TaskTemplate1", "Description", 3, 1)
+        taskTemplates.put(1, taskTemplate1)
+        val taskTemplate2 = TaskTemplate(2, getInvestmentManager(), "TaskTemplate2", "Description", 4, 1)
+        taskTemplates.put(2, taskTemplate2)
+        val taskTemplate3 = TaskTemplate(3, getInvestmentManager(), "TaskTemplate3", "Description", 2, 1)
+        taskTemplates.put(3, taskTemplate3)
+        val taskTemplate4 = TaskTemplate(4, getAccountant(), "TaskTemplate4", "Description", 6, 1)
+        taskTemplates.put(4, taskTemplate4)
+        val taskTemplate5 = TaskTemplate(5, getCustomerAdvisor(), "TaskTemplate5", "Description", 5, 1)
+        taskTemplates.put(5, taskTemplate5)
+
+        taskTemplate3.addPredecessor(taskTemplate1)
+        taskTemplate3.addPredecessor(taskTemplate2)
+        taskTemplate4.addPredecessor(taskTemplate3)
+        taskTemplate5.addPredecessor(taskTemplate3)
+
+        return taskTemplates
+    }
+
+    fun getProcessTemplate1(): ProcessTemplate {
+        return ProcessTemplate(
+            1,
+            "Testprocess 1",
+            "Description",
+            100,
+            karlCustomerAdvisor,
+            Instant.now(),
+            null,
+            0,
+            0,
+            false,
+            getTaskTemplatesSet1()
+        )
+    }
+
+    fun getProcessTemplate2(): ProcessTemplate {
+        return ProcessTemplate(
+            1,
+            "Testprocess 1",
+            "Description",
+            100,
+            kunigundeCustomerAdvisor,
+            Instant.now(),
+            null,
+            0,
+            0,
+            false,
+            getTaskTemplatesSet2()
+        )
     }
 
 }
