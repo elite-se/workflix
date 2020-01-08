506aa6d37c1b85100ab58cf6f43deaff59f6dbbc
Marvin Brieger <mail@marvinbrieger.de>
2020-01-08 15:02:51 +0100

    make ready for testing

--- workflix/backend/backend/src/main/kotlin/de/se/team3/logic/domain/ProcessGroup.kt
+++ workflix/backend/backend/src/main/kotlin/de/se/team3/logic/domain/ProcessGroup.kt
@@ -38,24 +38,43 @@ data class ProcessGroup(
     /**
      * Create-Constructor
      */
-    constructor(ownerID: String, title: String, description: String) :
-            this(null, UserContainer.getUser(ownerID), title, description, Instant.now(), false, ArrayList<User>()) {
-
+    constructor(
+        owner: User,
+        title: String,
+        description: String
+    ) : this(
+        null,
+        owner,
+        title,
+        description,
+        Instant.now(),
+        false,
+        ArrayList<User>()
+    ) {
         setTitle(title)
-        setOwnerById(ownerID)
     }
 
     /**
      * Update-Constructor
      */
-    constructor(id: Int, ownerId: String, title: String, description: String) :
-            this (id, UserContainer.getUser(ownerId), title, description, Instant.now(), false, ArrayList<User>()) {
-
+    constructor(
+        id: Int,
+        owner: User,
+        title: String,
+        description: String
+    ) : this (
+        id,
+        owner,
+        title,
+        description,
+        Instant.now(),
+        false,
+        ArrayList<User>()
+    ) {
         if (id < 1)
             throw InvalidInputException("id must be positive")
 
         setTitle(title)
-        setOwnerById(ownerId)
     }
 
     /**
@@ -82,18 +101,6 @@ data class ProcessGroup(
      */
     fun setDescription(description: String) {
         this.description = description
-    }
-
-    /**
-     * Sets the owner by the given id.
-     *
-     * @throws InvalidInputException Is thrown if ownerId is not a valid user id.
-     */
-    fun setOwnerById(ownerId: String) {
-        if (ownerId.length != 24)
-            throw InvalidInputException("invalid user id")
-
-        owner = UserContainer.getUser(ownerId)
     }
 
     /**
