2f6aa43b884a009e97453c2523b6ccdbebe74d54
Marvin Brieger <mail@marvinbrieger.de>
2020-01-08 11:15:44 +0100

    ktlint

--- workflix/backend/backend/src/main/kotlin/de/se/team3/logic/domain/User.kt
+++ workflix/backend/backend/src/main/kotlin/de/se/team3/logic/domain/User.kt
@@ -14,6 +14,7 @@ class User(
     val name: String,
     val displayname: String,
     val email: String,
+    @JsonIgnore
     var password: String,
     @JsonSerialize(using = InstantSerializer::class)
     val createdAt: Instant
