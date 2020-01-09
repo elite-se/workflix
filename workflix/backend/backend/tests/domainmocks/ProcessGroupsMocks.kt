c0212c30a8598c1743c5c2252c02716d7007cd25
Marvin Brieger <mail@marvinbrieger.de>
2020-01-09 18:12:53 +0100

    reorganized test mocks

---
+++ workflix/backend/backend/tests/domainmocks/ProcessGroupsMocks.kt
@@ -0,0 +1,31 @@
+package domainmocks
+
+import de.se.team3.logic.domain.ProcessGroup
+import de.se.team3.logic.domain.User
+import java.time.Instant
+
+object ProcessGroupsMocks {
+
+    fun getTestProcessGroupExtorel(): ProcessGroup {
+        val members = ArrayList<User>()
+        members.add(UsersAndRolesMocks.marvinBrieger)
+        members.add(UsersAndRolesMocks.michaelMarkl)
+        members.add(UsersAndRolesMocks.erikPallas)
+        members.add(UsersAndRolesMocks.karlCustomerAdvisor)
+
+        return ProcessGroup(1,
+            UsersAndRolesMocks.karlCustomerAdvisor, "Extorel", "Group Extorel", Instant.now(), false, members)
+    }
+
+    fun getTestProcessGroupFugger(): ProcessGroup {
+        val members = ArrayList<User>()
+        members.add(UsersAndRolesMocks.eliasKeis)
+        members.add(UsersAndRolesMocks.michaelMarkl)
+        members.add(UsersAndRolesMocks.erikPallas)
+        members.add(UsersAndRolesMocks.kunigundeCustomerAdvisor)
+
+        return ProcessGroup(2,
+            UsersAndRolesMocks.kunigundeCustomerAdvisor, "Fugger", "Group Fugger", Instant.now(), false, members)
+    }
+
+}
---
+++ workflix/backend/backend/tests/domainmocks/ProcessesMocks.kt
@@ -0,0 +1,27 @@
+package domainmocks
+
+import de.se.team3.logic.domain.Process
+import de.se.team3.logic.domain.ProcessStatus
+import de.se.team3.logic.domain.Task
+import de.se.team3.logic.domain.TaskAssignment
+import de.se.team3.logic.domain.TaskComment
+import java.time.Instant
+
+object ProcessesMocks {
+
+    fun getProcess1(): Process {
+        val groupExtorel = ProcessGroupsMocks.getTestProcessGroupExtorel()
+        val starter = UsersAndRolesMocks.karlCustomerAdvisor
+        val processTemplate = ProcessTemplatesMocks.getProcessTemplate1()
+
+        // generate tasks without ids from task template like in constructor of process
+        val tasksWithoutIds = Process.createTasks(processTemplate)
+        // enrich them with ids
+        val tasksWithIds = HashMap<Int, Task>()
+        for ((key, task) in tasksWithoutIds)
+            tasksWithIds.put(key, Task(key, key, task.startedAt, ArrayList<TaskComment>(), ArrayList<TaskAssignment>(), null))
+
+        return Process(1, starter, groupExtorel, processTemplate, "Testprocess 1", "Description", ProcessStatus.RUNNING, null, Instant.now(), tasksWithIds)
+    }
+
+}
