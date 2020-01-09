92ce758f186138d7475554d225e28c86e927da34
Elias Keis <elias.keis@student.uni-augsburg.de>
2020-01-09 13:44:17 +0100

    onAdd/onRemove/onClear for simple multiselect

--- workflix/frontend/src/routes/users/components/users/UserCardEdit.js
+++ workflix/frontend/src/routes/users/components/users/UserCardEdit.js
@@ -1,7 +1,6 @@
 // @flow
 
 import React from 'react'
-import { difference } from 'lodash'
 import type { ProcessGroupType } from '../../../../modules/datatypes/ProcessGroup'
 import type { UserRoleType, UserType } from '../../../../modules/datatypes/User'
 import TitledCard from '../TitledCard'
@@ -23,56 +22,65 @@ type PropsType = {|
 |}
 
 class UserCardEdit extends React.Component<PropsType> {
-  onSelectedGroupsChanged = (selectedGroups: ProcessGroupType[]) => {
-    const { user, processGroups } = this.props
-    const newGroups = selectedGroups.map(group => group.id)
-    const oldGroups = user.processGroupIds
-    const addedGroups = difference(newGroups, oldGroups)
-    const removedGroups = difference(oldGroups, newGroups)
-    addedGroups.map(id => processGroups.get(id)).filter(Boolean)
-      .forEach(group => new ProcessGroupsApi().addMembership(group.id, user.id)
-        .then(this.props.onGroupMembershipAdded(group, user))
-        .catch(toastifyError))
-    removedGroups.map(id => processGroups.get(id)).filter(Boolean)
-      .forEach(group => new ProcessGroupsApi().removeMembership(group.id, user.id)
-        .then(this.props.onGroupMembershipRemoved(group, user))
-        .catch(toastifyError))
+  onGroupAdded = (group: ProcessGroupType) => {
+    const { user } = this.props
+    new ProcessGroupsApi().addMembership(group.id, user.id)
+      .then(this.props.onGroupMembershipAdded(group, user))
+      .catch(toastifyError)
   }
 
-  onSelectedRolesChanged = (selectedRoles: UserRoleType[]) => {
-    const { user, roles } = this.props
-    const newRoles = selectedRoles.map(role => role.id)
-    const oldRoles = user.userRoleIds
-    const addedRoles = difference(newRoles, oldRoles)
-    const removedRoles = difference(oldRoles, newRoles)
-    addedRoles.map(id => roles.get(id)).filter(Boolean)
-      .forEach(role => new UsersApi().addRoleMembership(role.id, user.id)
-        .then(this.props.onRoleMembershipAdded(role, user))
-        .catch(toastifyError))
-    removedRoles.map(id => roles.get(id)).filter(Boolean)
-      .forEach(role => new UsersApi().removeRoleMembership(role.id, user.id)
-        .then(this.props.onRoleMembershipRemoved(role, user))
-        .catch(toastifyError))
+  onGroupRemoved = (group: ProcessGroupType) => {
+    const { user } = this.props
+    new ProcessGroupsApi().removeMembership(group.id, user.id)
+      .then(this.props.onGroupMembershipRemoved(group, user))
+      .catch(toastifyError)
   }
+
+  onGroupsCleared = () => this.getSelectedGroups().forEach(group => this.onGroupRemoved(group))
+
+  onRoleAdded = (role: UserRoleType) => {
+    const { user } = this.props
+    new UsersApi().addRoleMembership(role.id, user.id)
+      .then(this.props.onRoleMembershipAdded(role, user))
+      .catch(toastifyError)
+  }
+
+  onRoleRemoved = (role: UserRoleType) => {
+    const { user } = this.props
+    new UsersApi().removeRoleMembership(role.id, user.id)
+      .then(this.props.onRoleMembershipRemoved(role, user))
+      .catch(toastifyError)
+  }
+
+  onRolesCleared = () => this.getSelectedRoles().forEach(role => this.onRoleRemoved(role))
+
+  getSelectedGroups = () => this.props.user.processGroupIds.map(id => this.props.processGroups.get(id)).filter(Boolean)
+  getSelectedRoles = () => this.props.user.userRoleIds.map(id => this.props.roles.get(id)).filter(Boolean)
 
   render () {
     const { user, processGroups, roles } = this.props
-    const selectedGroups = this.props.user.processGroupIds.map(id => this.props.processGroups.get(id)).filter(Boolean)
-    const selectedRoles = this.props.user.userRoleIds.map(id => this.props.roles.get(id)).filter(Boolean)
     return <TitledCard key={user.id} title={user.name} elevation={Elevation.FOUR}>
       <IconRow icon='person'>{user.displayname}</IconRow>
       <IconRow icon='envelope'><a href={`mailto:${user.email}`}>{user.email}</a></IconRow>
       <IconRow icon='office'>
-        <SimpleMultiSelect items={Array.from(processGroups.values())} selection={selectedGroups}
-                           onSelectionChanged={this.onSelectedGroupsChanged}
-                           multiSelectProps={{ fill: true, popoverProps: { usePortal: false } }}
-                           toID={this.getGroupId} render={this.getGroupTitle}/>
+        <SimpleMultiSelect items={Array.from(processGroups.values())} selection={this.getSelectedGroups()}
+                           multiSelectProps={{
+                             fill: true,
+                             popoverProps: { usePortal: false }
+                           }}
+                           toID={this.getGroupId} render={this.getGroupTitle}
+                           onItemAdded={this.onGroupAdded} onItemRemoved={this.onGroupRemoved}
+                           onItemsCleared={this.onGroupsCleared}/>
       </IconRow>
       <IconRow icon='badge'>
-        <SimpleMultiSelect items={Array.from(roles.values())} selection={selectedRoles}
-                           onSelectionChanged={this.onSelectedRolesChanged}
-                           multiSelectProps={{ fill: true, popoverProps: { usePortal: false } }}
-                           toID={this.getRoleId} render={this.getRoleName}/>
+        <SimpleMultiSelect items={Array.from(roles.values())} selection={this.getSelectedRoles()}
+                           multiSelectProps={{
+                             fill: true,
+                             popoverProps: { usePortal: false }
+                           }}
+                           toID={this.getRoleId} render={this.getRoleName}
+                           onItemAdded={this.onRoleAdded} onItemRemoved={this.onRoleRemoved}
+                           onItemsCleared={this.onRolesCleared}/>
       </IconRow>
     </TitledCard>
   }
