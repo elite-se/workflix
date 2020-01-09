993f325eb0cb7984694af3ea5f22c4cb5cdc2781
Erik Pallas <erik.pallas@student.uni-augsburg.de>
2020-01-09 18:37:30 +0100

    connect to TUM Mail Server

--- workflix/backend/backend/src/main/kotlin/de/se/team3/logic/authentication/VerificationMailManager.kt
+++ workflix/backend/backend/src/main/kotlin/de/se/team3/logic/authentication/VerificationMailManager.kt
@@ -6,28 +6,38 @@ import javax.mail.internet.MimeMessage
 
 object VerificationMailManager {
 
-
-    fun sendVerificationMail(email: String) {
-        val to = email
+    /**
+     * Sends an E-Mail with a verification code to the given mail address.
+     *
+     * @param to mail address the verification is going to be sent to
+     */
+    fun sendVerificationMail(to: String) {
         val from = "verification@workflix.com"
         val host = "postout.lrz.de"
+        val port = "587"
 
         //Get the session object
-        val properties = System.getProperties()
-        properties.setProperty("mail.smtp.host", host)
-        val session = Session.getDefaultInstance(properties)
+        val mailProperties = System.getProperties()
+        mailProperties.setProperty("mail.smtp.host", host)
+        mailProperties.setProperty("mail.smtp.port", port)
+        mailProperties.put("mail.smtp.auth", true)
+        mailProperties.put("mail.smtp.socketFactory.port", port)
+        mailProperties.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory")
+        mailProperties.put("mail.smtp.socketFactory.fallback", "false")
+        mailProperties.put("mail.smtp.starttls.enable", "true")
+        val session = Session.getDefaultInstance(mailProperties)
 
         //compose the message
-        try{
+        try {
             val message = MimeMessage(session)
             message.setFrom(InternetAddress(from))
             message.addRecipient(Message.RecipientType.TO,InternetAddress(to))
             message.subject = "Ping"
-            message.setText("Hello, this is example of sending email  ")
+            message.setText("Hello, this is a mailing ping test.")
 
             // Send message
             Transport.send(message)
-            println("message sent successfully....")
+            println("message sent successfully...")
 
         } catch (mex: MessagingException) {mex.printStackTrace()}
     }
