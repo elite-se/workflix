49f54eca12279a70a274d4d553a266dd0a51b1b8
Erik Pallas <erik.pallas@student.uni-augsburg.de>
2020-01-09 10:05:39 +0100

    reset backend location

--- workflix/backend/backend/src/main/kotlin/de/se/team3/webservice/handlers/CORSHandler.kt
+++ workflix/backend/backend/src/main/kotlin/de/se/team3/webservice/handlers/CORSHandler.kt
@@ -3,9 +3,11 @@ package de.se.team3.webservice.handlers
 import io.javalin.http.Context
 
 object CORSHandler {
+    /**
+     * Handles CORS OPTIONS requests: replies with status code 200 if authorization header is requested.
+     */
     fun optionsRequest(ctx: Context) {
         if (ctx.header("Access-Control-Request-Headers") == "authorization")
             ctx.status(200)
     }
-
 }
