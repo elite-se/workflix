c498c03bd00c98b5dfd3ce1755026c6d0b50f7fb
Erik Pallas <erik.pallas@student.uni-augsburg.de>
2020-01-09 09:13:27 +0100

    got linted

--- workflix/backend/backend/src/main/kotlin/de/se/team3/logic/authentication/LoginManager.kt
+++ workflix/backend/backend/src/main/kotlin/de/se/team3/logic/authentication/LoginManager.kt
@@ -3,7 +3,6 @@ package de.se.team3.logic.authentication
 import de.se.team3.logic.container.UserContainer
 import de.se.team3.logic.domain.User
 import de.se.team3.logic.exceptions.InvalidInputException
-import de.se.team3.logic.exceptions.NotAuthorizedException
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 
 object LoginManager {
