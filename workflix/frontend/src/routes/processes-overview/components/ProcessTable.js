8b26205e7c46da1a88e0279819a27707a2ce5388
Michael Markl <marklmichael98@gmail.com>
2020-01-09 18:08:54 +0100

    Add simple table for processes

---
+++ workflix/frontend/src/routes/processes-overview/components/ProcessTable.js
@@ -0,0 +1,86 @@
+// @flow
+
+import React from 'react'
+import type { ProcessType } from '../../../modules/datatypes/Process'
+import ProcessApi from '../../../modules/api/ProcessApi'
+import { Colors, HTMLTable } from '@blueprintjs/core'
+import styled from 'styled-components'
+import type { UserType } from '../../../modules/datatypes/User'
+import withPromiseResolver from '../../../modules/app/hocs/withPromiseResolver'
+import type { FiltersType } from '../../../modules/datatypes/Filters'
+import type { ProcessGroupType } from '../../../modules/datatypes/ProcessGroup'
+import ProcessProgress from '../../../modules/common/components/ProcessProgress'
+
+const ProcessTableWrapper = styled<{}, {}, 'div'>('div')`
+  display: flex;
+  flex: 1;
+  justify-content: center;
+  flex-direction: row;
+`
+
+type PropsType = {|
+  filters: FiltersType,
+  processes: Array<ProcessType>,
+  processGroups: Map<number, ProcessGroupType>,
+  users: Map<string, UserType>
+|}
+
+class ProcessList extends React.Component<PropsType> {
+  render () {
+    const { processes, users, processGroups } = this.props
+    return <div style={{
+      maxWidth: '100%',
+      overflowX: 'auto',
+      display: 'flex'
+    }}>
+      <ProcessTableWrapper>
+        {
+          processes.length === 0
+            ? <span style={{
+              color: Colors.GRAY2,
+              alignItem: 'stretch'
+            }}>There are no processes matching the filters.</span>
+            : <HTMLTable bordered striped interactive style={{ width: '100%' }}>
+              <thead>
+              <th>Id</th>
+              <th>Title</th>
+              <th>Started at</th>
+              <th>Progress</th>
+              <th>Process Group</th>
+              <th>Starter</th>
+              </thead>
+              <tbody>
+              {processes.map(process => (
+                <tr key={process.id}>
+                  <td>{process.id}</td>
+                  <td>{process.title}</td>
+                  <td>{process.startedAt.toLocaleString()}</td>
+                  <td><ProcessProgress process={process}/></td>
+                  <td>{processGroups.get(process.processGroupId)?.title || ''}</td>
+                  <td>{users.get(process.starterId)?.name || ''}</td>
+                </tr>
+              ))}
+              </tbody>
+            </HTMLTable>
+        }</ProcessTableWrapper>
+    </div>
+  }
+}
+
+const promiseCreator = (props: *, refresh) =>
+  new ProcessApi().getProcesses(props.filters)
+    .then(processes => Promise.all([
+      Promise.resolve(processes),
+      new ProcessApi().getTaskTemplatesForProcessTemplates(processes.map(proc => proc.processTemplateId))
+    ]))
+    .then(
+      ([processes, taskTemplates]) => ({
+        processes,
+        taskTemplates,
+        refresh
+      })
+    )
+
+const shouldUpdate = (oldProps: *, newProps: *) => oldProps.filters !== newProps.filters
+
+export default withPromiseResolver<PropsType, *>(promiseCreator, shouldUpdate)(ProcessList)
--- workflix/frontend/src/routes/processes-overview/components/ProcessesOverview.js
+++ workflix/frontend/src/routes/processes-overview/components/ProcessesOverview.js
@@ -1,5 +1,53 @@
 // @flow
 
 import React from 'react'
+import UserApi from '../../../modules/api/UsersApi'
+import type { UserType } from '../../../modules/datatypes/User'
+import withPromiseResolver from '../../../modules/app/hocs/withPromiseResolver'
+import type { FiltersType } from '../../../modules/datatypes/Filters'
+import ProcessGroupsApi from '../../../modules/api/ProcessGroupsApi'
+import type { ProcessGroupType } from '../../../modules/datatypes/ProcessGroup'
+import Filters from '../../tasks/components/filtering/Filters'
+import ProcessTable from './ProcessTable'
 
-export default () => <div/>
+type PropsType = {|
+  users: Map<string, UserType>,
+  processGroups: Map<number, ProcessGroupType>
+|}
+type StateType = {|
+  filters: FiltersType
+|}
+
+class TasksOverview extends React.Component<PropsType, StateType> {
+  state = {
+    filters: {}
+  }
+
+  onFiltersChanged = (newFilters: FiltersType) => {
+    this.setState({ filters: newFilters })
+  }
+
+  render () {
+    const { users, processGroups } = this.props
+    const { filters } = this.state
+    return <div style={{
+      maxWidth: '100%',
+      boxSizing: 'border-box',
+      flex: 1
+    }}>
+      <Filters onFiltersChanged={this.onFiltersChanged} filters={filters}
+               users={users} processGroups={processGroups}/>
+      <ProcessTable filters={filters} users={users} processGroups={processGroups}/>
+    </div>
+  }
+}
+
+const promiseCreator = () => Promise.all([
+  new UserApi().getUsers(),
+  new ProcessGroupsApi().getProcessGroups()
+]).then(([users, processGroups]) => ({
+  users,
+  processGroups
+}))
+
+export default withPromiseResolver<PropsType, *>(promiseCreator)(TasksOverview)
