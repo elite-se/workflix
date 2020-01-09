1678aa48c4ebb48f5152be74359f5aa5328b7e43
Erik Pallas <erik.pallas@student.uni-augsburg.de>
2020-01-10 00:03:26 +0100

    fts just user SendGrid - implemented VerificationMailManager

--- workflix/backend/backend/src/main/kotlin/de/se/team3/logic/authentication/VerificationMailManager.kt
+++ workflix/backend/backend/src/main/kotlin/de/se/team3/logic/authentication/VerificationMailManager.kt
@@ -1,48 +1,82 @@
 package de.se.team3.logic.authentication
 
-import javax.mail.*
-import javax.mail.internet.InternetAddress
-import javax.mail.internet.MimeMessage
+import com.sendgrid.Method
+import com.sendgrid.Request
+import com.sendgrid.Response
+import com.sendgrid.SendGrid
+import com.sendgrid.helpers.mail.Mail
+import com.sendgrid.helpers.mail.objects.Content
+import com.sendgrid.helpers.mail.objects.Email
+import de.se.team3.logic.exceptions.InvalidInputException
+import de.se.team3.logic.***REMOVED***connector.UserQuerying
+import java.io.IOException
 
 object VerificationMailManager {
+
+    //maps each mail address to its generated key
+    //if the server resets, the key expires
+    //TODO more secure expiring method
+    private val keyMap = HashMap<String, String>()
+
+    //API KEY necessary for mailing via SendGrid
+    //TODO store somewhere safe
+    private const val sendGridApiKey = "***REMOVED***"
+
+    /**
+     * Generates a verification key and sends it to the respective user.
+     *
+     * @param mail email address of the user trying to sign up
+     */
+    fun initVerification(mail: String) {
+        if (UserQuerying.searchFor***REMOVED***User(mail) == null)
+            throw InvalidInputException("There is no user with this email address.")
+        val charPool: List<Char> = ('A'..'Z') + ('0'..'9')
+        val key = (1..8)
+            .map { kotlin.random.Random.nextInt(0, charPool.size) }
+            .map(charPool::get)
+            .joinToString("")
+        keyMap[mail] = key
+        sendVerificationMail(mail, key)
+    }
+
+    fun isValidForUser(mail: String, key: String): Boolean {
+        return keyMap[mail] == key
+    }
 
     /**
      * Sends an E-Mail with a verification code to the given mail address.
      *
-     * @param to mail address the verification is going to be sent to
+     * @param toAddress mail address the verification is going to be sent to
+     * @param key generated verification key
      */
-    fun sendVerificationMail(to: String) {
-        val from = "verification@workflix.com"
-        val host = "postout.lrz.de"
-        val port = "587"
+    private fun sendVerificationMail(toAddress: String, key: String) {
+        val sendgrid = SendGrid(sendGridApiKey)
 
-        //Get the session object
-        val mailProperties = System.getProperties()
-        mailProperties.setProperty("mail.smtp.host", host)
-        mailProperties.setProperty("mail.smtp.port", port)
-        mailProperties.put("mail.smtp.auth", true)
-        mailProperties.put("mail.smtp.socketFactory.port", port)
-        mailProperties.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory")
-        mailProperties.put("mail.smtp.socketFactory.fallback", "false")
-        mailProperties.put("mail.smtp.starttls.enable", "true")
-        val session = Session.getDefaultInstance(mailProperties)
+        val from = Email("verification@workflix.com")
+        val to = Email(toAddress)
+        val subject = "Sending with SendGrid is Fun"
+        val content = Content("text/html", """<h1 style="color: #5e9ca0;"><span style="color: #808080;">Thank you for using Workflix!<span style="color: #ffffff;"></span></span></h1>
+<p>In order to start working, you have to verify your account by entering your verification code.</p>
+<p>Your verification code is:</p>
+<h3 style="padding-left: 60px;"><span style="color: #808080;"><strong>$key</strong></span></h3>
+<p>This code will expire in several minutes.</p>
+<p>If your code has expired for any reason whatsoever, simply restart the registration process and a new code will be sent to you.</p>
+<p>&nbsp;</p>
+<p>Your Workflix Team</p>
+<p><strong>&nbsp;</strong></p>""")
+        val mail = Mail(from, subject, to, content)
 
-        //compose the message
+        val request = Request()
         try {
-            val message = MimeMessage(session)
-            message.setFrom(InternetAddress(from))
-            message.addRecipient(Message.RecipientType.TO,InternetAddress(to))
-            message.subject = "Ping"
-            message.setText("Hello, this is a mailing ping test.")
-
-            // Send message
-            Transport.send(message)
-            println("message sent successfully...")
-
-        } catch (mex: MessagingException) {mex.printStackTrace()}
+            request.setMethod(Method.POST)
+            request.setEndpoint("mail/send")
+            request.setBody(mail.build())
+            val response: Response = sendgrid.api(request)
+            println(response.statusCode)
+            println(response.body)
+            println(response.headers)
+        } catch (ex: IOException) {
+            throw ex
+        }
     }
-}
-
-fun main() {
-    VerificationMailManager.sendVerificationMail("erik.pallas@student.uni-augsburg.de")
 }
