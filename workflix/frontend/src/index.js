23fd8f48e653a90ef2b32e6196894ff528761076
Michael Markl <marklmichael98@gmail.com>
2020-01-09 17:55:20 +0100

    Remove blueprint js table again

--- workflix/frontend/src/index.js
+++ workflix/frontend/src/index.js
@@ -6,7 +6,6 @@ import '@blueprintjs/core/lib/css/blueprint.css'
 import '@blueprintjs/icons/lib/css/blueprint-icons.css'
 import '@blueprintjs/datetime/lib/css/blueprint-datetime.css'
 import '@blueprintjs/select/lib/css/blueprint-select.css'
-import '@blueprintjs/table/lib/css/table.css'
 
 import App from './App'
 import * as serviceWorker from './serviceWorker'
