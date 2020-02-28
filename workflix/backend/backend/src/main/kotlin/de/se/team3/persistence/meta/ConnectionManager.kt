6524976f85dc60d0867a73601251be3fd66c6dc9
Elias Keis <elias.keis@student.uni-augsburg.de>
2020-02-28 11:27:35 +0100

    move database credentials into environment variables

--- workflix/backend/backend/src/main/kotlin/de/se/team3/persistence/meta/ConnectionManager.kt
+++ workflix/backend/backend/src/main/kotlin/de/se/team3/persistence/meta/ConnectionManager.kt
@@ -6,14 +6,11 @@ import me.liuwj.ktorm.support.postgresql.PostgreSqlDialect
 object ConnectionManager {
 
     fun connect() {
-        val server = "***REMOVED***"
-        val database = "***REMOVED***"
-        val user = "***REMOVED***"
-        val password = "***REMOVED***"
-        Database.connect(
-            "jdbc:postgresql://" + server + "/" + database + "?user=" + user + "&password=" + password,
-            driver = "org.postgresql.Driver",
-            dialect = PostgreSqlDialect()
-        )
+        val server = System.getenv("workflix.db.server")
+        val database = System.getenv("workflix.db.database")
+        val user = System.getenv("workflix.db.user")
+        val password = System.getenv("workflix.db.password")
+        val url = "jdbc:postgresql://$server/$database"
+        Database.connect(url, driver = "org.postgresql.Driver", dialect = PostgreSqlDialect(), user = user, password = password)
     }
 }
